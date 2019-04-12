6089ad8da8be4455667d21e277f36c560b23802b
Mengzhennn <48179897+Mengzhennn@users.noreply.github.com>
2019-04-11 20:27:31 -0400

    edited main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,7 @@
-#include "memory.cpp"
-#include "encode.cpp"
-
-#include <string>
-#include <fstream>
-#include <sstream>
 #include <iostream>
-#include <stdio.h>
-
-using namespace std;
+#include "memory.hpp"
+#include "structs.hpp"
+#include "encode.hpp"
 
 string file_to_string(ifstream& in){
     stringstream sstr;
@@ -15,17 +9,109 @@ string file_to_string(ifstream& in){
     return sstr.str();
 }
 
-ifstream infile("input.txt");
-string code = file_to_string(infile);
-memory external = memory(2048);
-vector<string> binary = string_to_binary(code);
+int main(int argc, char** argv) 
+{
+	bool run_prog = true;
+	std::string Source_In;
+	string code;
+	std::string inputFile = "input2.txt";
+	
+	{
+		ifstream infile(inputFile.c_str());
+		code = file_to_string(infile);
+		infile.close();
+	}
+	
+	
+	memory external = memory(2048);
+	vector<string> binary = string_to_binary(code);
+	
+	while (run_prog)
+	{
+		
+		cout<<"Enter number for command:1. edit source code; 2. print converted assembly code; 3. memory; 4. exit" <<endl;
+		
+		string command;
 
-int main(){
+		getline(std::cin, command);
+		
+		if(command == "1")
+		{
+			std::cout << "Please edit the input file " << inputFile << " directly, press any key to continue" << std::endl;
+			std::cin.ignore(1);
+			std::cin.clear();
+			
+			ifstream infile(inputFile.c_str());
+			code = file_to_string(infile);
+			infile.close();
+		}
+		else if(command=="2" )
+	
+		{
+			if(Source_In != "")
+			{
+				//parse entire string, including tabs and the like, into MIPS assembly code.
+				//Call function on Source_In string.
+				//Store result in some container.
+			}
+			//call function that print converted assembly code
+			//Print using cout or in-built print function.
+		}
+		
+		else if(command == "3")
+	
+		{
+				for (int j = 0; j < binary.size(); j++){
+					external.setword(j, binary[j]);
+				}
+				cout << "| Word | Data |" << "\n";
+	
+				//code doesn't work on my mac without the .c_str() at the end. If this causes
+				//a problem removing it should hopefully fix it - Emile
+				for (int i = 0; i < external.getsize(); i++)
+				{
+					
+					printf ("|  %i   | %s |\n", i, external.getword(i).c_str()); 
+				}
+	
+	
+
+		}
+		
+		else if (command =="4")
+	
+		{
+	
+			run_prog = false;
+			
+
+		}
+		
+		else
+
+		{
+
+			cout<<"Please enter a valid command"<<endl;
+			
+		}
+	
+	}
+	
+	return 0;
+}
+
+
+
+/*
 	for (int j = 0; j < binary.size(); j++){
 		external.setword(j, binary[j]);
 	}
+	
 	cout << "| Word | Data |" << "\n";
+	
+	//code doesn't work on my mac without the .c_str() at the end. If this causes
+	//a problem removing it should hopefully fix it - Emile
 	for (int i = 0; i < external.getsize(); i++){
-		printf ("|  %i   | %s |\n", i, external.getword(i));
+		printf ("|  %i   | %s |\n", i, external.getword(i).c_str());   
 	}
-}
+*/
